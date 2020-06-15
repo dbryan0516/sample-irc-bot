4ccef1420fefefe0ab837a8f63010abf7b98adde
Dylan Bryan <dylan.bryan@consensys.net>
2020-06-15 17:56:01 -0400

    Added time popularity and state saver

--- index.js
+++ index.js
@@ -11,20 +11,62 @@ const bot = new irc.Client(config.server, config.botName, {
 
 // IRC error handler
 bot.addListener('error', function(message) {
-  console.log('error: ', message);
+  console.log('IRC error: ', message);
 });
 
-// Listen for any message, say to him/her in the room
+// Listen for any message and only respond to commands in a channel
+// commands are in form "!command commandText"
 bot.addListener("message", function(from, to, text, message) {
-  timeController.getTimeForLocation('America/New_York').then((response)  => {
-    if (to === config.botName) {
-      bot.say(from, response); // pm
-    } else {
-      bot.say(to, response); // channel
+  if (to !== config.botName) {
+    let words = text.split(" ");
+    if (words.length <= 1 || words[0][0] !== '!') {
+      return; // not a command
     }
+
+    switch (words[0]) {
+      case '!timeat':
+        handleTimeAtCommand(to, words);
+        break;
+      case '!timepopularity' :
+        console.log(words[0]);
+        handleTimePopularityCommand(to, words);
+        break;
+      default:
+        return; // unsupported command, do nothing
+    }
+  }
+});
+
+function handleTimePopularityCommand(to, words) {
+  let popularity = timeController.getPopularityForLocation(words[1]);
+  bot.say(to, popularity); // channel response
+}
+
+function handleTimeAtCommand(to, words) {
+  timeController.getTimeForLocation(words[1]).then((response) => {
+    bot.say(to, response); // channel response
   }).catch((err) => {
     console.log('error in index.js::message handler', err)
   });
+}
+
+// catch sigint to save state
+// special case for windows
+if (process.platform === "win32") {
+  var rl = require("readline").createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+
+  rl.on("SIGINT", function () {
+    process.emit("SIGINT");
+  });
+}
+
+process.on("SIGINT", function () {
+  // save state on exit
+  timeController.saveStateFile();
+  process.exit();
 });
 
 console.log("Listening for commands on " + config.channels.toString());
--- timeController.js
+++ timeController.js
@@ -1,26 +1,65 @@
 const request = require("request-promise");
+const fs = require("fs");
 
-// TODO: make this pull from TIME_API env var
-const apiBaseUrl = "http://worldtimeapi.org/api/timezone/";
+const API_URL = process.env.TIME_API || "http://worldtimeapi.org/api/timezone/";
+const STATE_FILE_PATH  = process.env.STATE_FILE_PATH || "./timepopularity.json";
 
 class TimeController {
 
   constructor() {
     this.months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+    this.state = this.getSavedState();
+  }
+
+  getSavedState () {
+    let filepath = STATE_FILE_PATH;
+    if (!fs.existsSync(filepath)) {
+      // no previous state found
+      return {};
+    }
+
+    let data = fs.readFileSync(filepath);
+    return JSON.parse(data);
+  }
+
+  updateState(timezone) {
+    timezone = timezone.toLowerCase();
+    console.log(timezone);
+    let parts =  timezone.split("/");
+    let i = 0;
+    let zone = "";
+    while(i < parts.length) {
+      zone += parts[i];
+      console.log(i, zone);
+      if (this.state.hasOwnProperty(zone)) {
+        this.state[zone] += 1;
+      } else {
+        this.state[zone] = 1;
+      }
+
+      zone += "/";
+      i++;
+    }
+  }
+
+  saveStateFile () {
+    fs.writeFileSync(STATE_FILE_PATH, JSON.stringify(this.state));
   }
 
   async getTimeForLocation(location) {
-    //  TODO: log location in db
-    let uri = apiBaseUrl + location;
+    let uri = API_URL + location;
     console.log('location', uri);
 
     let options = {method: 'GET', uri: uri};
     return request(options).then((response) => {
-
+      this.updateState(location); // update the count if the request was successful
       let timeObject = JSON.parse(response);
-      let time = new Date(Date.parse(timeObject['utc_datetime']));
+      let dateTime = timeObject['datetime'];
+
+      // remove the offset eg. '-07:00'
+      let dateTimeNoOffset = dateTime.substring(0, dateTime.length - 6);
+      let time = new Date(dateTimeNoOffset);
       let timeString = `${time.getDate()} ${this.months[time.getMonth()]} ${time.getFullYear()} ${time.getHours()}:${time.getMinutes()}`;
-      console.log(timeString);
       return timeString;
 
     }).catch((err) => {
@@ -29,7 +68,13 @@ class TimeController {
     });
   }
 
+  getPopularityForLocation(location) {
+    location = location.toLowerCase();
+    if (this.state.hasOwnProperty(location)) {
+      return this.state[location];
+    }
+    return 0;
+  }
 }
-
 
 module.exports = TimeController;
